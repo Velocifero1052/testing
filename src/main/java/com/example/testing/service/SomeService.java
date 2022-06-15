0c1ef132cc1cc8fcf710e7345f9305dedd1808f4
Velocifero1052 <commelevent1052@yandex.ru>
2022-06-15 22:08:22 +0500

    spring boot unit testing support chapter

--- src/main/java/com/example/testing/service/SomeService.java
+++ src/main/java/com/example/testing/service/SomeService.java
@@ -1,4 +1,7 @@
 package com.example.testing.service;
 
 public interface SomeService {
+
+    void someMethod();
+
 }
--- src/test/java/com/example/testing/ApplicationExampleTest.java
+++ src/test/java/com/example/testing/ApplicationExampleTest.java
@@ -1,15 +1,30 @@
 package com.example.testing;
 
+import com.example.testing.config.SomeConfig;
+import com.example.testing.service.SomeService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ApplicationExampleTest {
 
-    @Test
-    void exampleTest(){
-        System.out.println("omg");
+    private final SomeConfig someConfig;
+    private final SomeService someService;
+
+    @Autowired
+    public ApplicationExampleTest(SomeConfig someConfig, SomeService someService){
+        this.someConfig = someConfig;
+        this.someService = someService;
     }
 
+    @Test
+    void exampleTest() {
+        System.out.println(someConfig.someName);
+        someService.someMethod();
+        System.out.println("omg");
+    }
 
 }
